0304df3f6e9e282335d9c0c5c5827bda59d197c8
Julia <yulya.gorokhova.85@mail.ru>
2022-12-20 00:20:39 +0300

    Выводит большее число

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+WriteLine("Введите число c: ");
+int c = int.Parse(ReadLine()!);
+int max = 0;
+
+if (a > b)
+{
+   max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    WriteLine($"{ max }");
+}
+else
+{
+    WriteLine($"{ c }");
+}
