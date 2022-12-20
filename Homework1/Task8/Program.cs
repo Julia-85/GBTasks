739d7d3f5f9def11690dd2d167060e212f489f27
Julia <yulya.gorokhova.85@mail.ru>
2022-12-20 22:13:17 +0300

    Четные числа

---
+++ Homework1/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+WriteLine("Введите число:");
+int n = int.Parse(ReadLine()!);
+int a = 1;
+
+while ( a < n )
+{
+    if ( a % 2 == 0)
+    Write($"{a} ");
+    a = a + 1;
+}
