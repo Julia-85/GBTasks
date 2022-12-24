a9ce9d08c8fbafb5dd1ba9faa17ebf0022390aa3
Julia <yulya.gorokhova.85@mail.ru>
2022-12-24 21:52:16 +0300

    Дни недели

---
+++ Homework1/Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+WriteLine("Введите номер дня недели:");
+int a = int.Parse(ReadLine()!);
+
+if (a>0 & a<6)
+{
+    WriteLine("нет");
+}
+else if (a>5 & a<8)
+{
+    WriteLine("да");
+}
+else WriteLine("Неверное число");
