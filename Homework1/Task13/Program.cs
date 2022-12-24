16bdb0ad30074ed4dddaa8236f53d106c87d1add
Julia <yulya.gorokhova.85@mail.ru>
2022-12-24 23:39:25 +0300

    Нахождение третьего числа

---
+++ Homework1/Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a > 99)
+{
+ while (a > 999)
+{
+     a = a/10;
+}
+WriteLine($"{a%10}");
+}
+else WriteLine("Третьего числа нет");
