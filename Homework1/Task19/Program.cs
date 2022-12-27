224f3cf6b09e3dc1d5192bac7755c03407642ec7
Julia <yulya.gorokhova.85@mail.ru>
2022-12-28 01:07:09 +0300

    Палиндром

---
+++ Homework1/Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+WriteLine("Введите пятизначное число: ");
+int n = int.Parse(ReadLine()!);
+
+int a = n/10000;
+int b = n/1000%10;
+int c = n/10%10;
+int d = n%10;
+
+if ((a == d) & (b == c))
+// if ((n/10000 == n%10) & (n/1000%10 == n/10%10))
+{
+     WriteLine("да");
+}
+
+else
+{
+     WriteLine("нет");
+}
+
