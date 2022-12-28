339d3b5e6a05c5e9ed31bfa3fa035c806243cc53
Julia <yulya.gorokhova.85@mail.ru>
2022-12-29 00:43:18 +0300

    Расстояние между точками

---
+++ Homework1/Task21/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using static System.Console;
+
+Write("Точка a - Введите координату х: ");
+int.TryParse(ReadLine(), out int ax);
+Write("Точка a - Введите координату y: ");
+int.TryParse(ReadLine(), out int ay);
+Write("Точка a - Введите координату z: ");
+int.TryParse(ReadLine(), out int az);
+Write("Точка b - Введите координату x: ");
+int.TryParse(ReadLine(), out int bx);
+Write("Точка b - Введите координату y: ");
+int.TryParse(ReadLine(), out int by);
+Write("Точка b - Введите координату z: ");
+int.TryParse(ReadLine(), out int bz);
+
+double distance = Math.Sqrt(Math.Pow((ax-bx),2)+Math.Pow((ay-by),2)+Math.Pow((az-bz),2));
+WriteLine($"{distance:f2}");
