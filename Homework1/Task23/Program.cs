903b125f18c30c24095770988f508db749d9bbc8
Julia <yulya.gorokhova.85@mail.ru>
2022-12-29 00:19:57 +0300

    Куб числа

---
+++ Homework1/Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+
+WriteLine ("Введите число: ");
+int.TryParse(ReadLine(), out int n);
+int a = 1;
+
+while(a < n+1)
+{
+    WriteLine($"Куб числа {a} = {Math.Pow(a,3)}");
+    a++;
+}
