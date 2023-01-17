666cc2d525257c647a7c2bf232f21c68a4fe2020
Julia <yulya.gorokhova.85@mail.ru>
2023-01-17 23:23:15 +0300

    Возведение в степень

---
+++ Homework1/Tasks25/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, реализующую метод,который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B с использованием цикла.
+
+using static System.Console;
+WriteLine("Введите число А: ");
+int A = int.Parse(ReadLine()!);
+WriteLine("Введите число В: ");
+int B = int.Parse(ReadLine()!);
+
+int sum = DegNum(A, B);
+WriteLine($"{sum}");
+
+
+
+
+
+
+
+int DegNum(int A, int B)
+{ 
+    int result=1;
+    int i=1;
+    while(B>=i)
+    {
+        result*= A;
+             i++;
+    }
+    return result;
+}
+
+
