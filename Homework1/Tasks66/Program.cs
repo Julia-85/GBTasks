95cbb9a366e2f4ad18346c3272fbb1354e44bc57
Julia <yulya.gorokhova.85@mail.ru>
2023-02-06 19:28:52 +0300

    Сумма элементов

---
+++ Homework1/Tasks66/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая реализует рекурсивный метод 
+нахождения суммы натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+using static System.Console;
+
+WriteLine("Ведите число m:  ");
+int m = int.Parse(ReadLine()!);
+WriteLine("Введите число n:  ");
+int n = int.Parse(ReadLine()!);
+  if (m>n)
+    {
+        int temp=0;
+        temp=m;
+        m=n;
+        n=temp;  
+    }
+
+
+
+void GetSumm (int m, int n, int summ)
+{
+    if (m > n) 
+    {
+        WriteLine($"Сумма : {summ}"); 
+        return;
+    }
+    summ = summ + (m++);
+    GetSumm(m, n, summ);
+}
+
+GetSumm(m, n, 0);
+
